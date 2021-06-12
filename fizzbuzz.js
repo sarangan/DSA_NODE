67ac9b669c02550a4db82eeb4e6cbcd755ee0d0d
saran <sarangan12@gmail.com>
2021-06-12 22:57:35 +0800

    Create fizzbuzz.js

---
+++ fizzbuzz.js
@@ -0,0 +1,19 @@
+function fizzBuzz(n) {
+    
+  if(n === 0 ){
+   return n; 
+  }else if(n % 3 === 0 && n % 5 === 0){
+        return 'fizzbuzz';
+  }
+  else if(n % 3 === 0){
+      return 'fizz';
+  }
+  else if(n % 5 === 0){
+      return 'buzz';
+  }
+  else {
+    return 0;
+  }
+}
+
+fizzBuzz(0);
