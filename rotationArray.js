11b225195ccfc5b8b048196a0cfa64e38ab9b8da
saran <sarangan12@gmail.com>
2021-09-26 05:59:45 +0800

    Create rotationArray.js

---
+++ rotationArray.js
@@ -0,0 +1,20 @@
+function is_rotation(list1, list2){
+  let pointer = 0;
+  
+  for(let i = 0; i < list2.length; i++){
+    if(list2[i] === list1[0]){
+      pointer = i;
+      break;
+    }
+  }
+  
+  const temp = [...list2.slice(pointer), ...list2.slice(0, pointer)];
+  
+  return list1.join() === temp.join();
+}
+
+
+const list1 = [1, 2, 3, 4, 5, 6, 7]
+const list2 = [7, 1, 2, 3, 4, 5, 6]
+
+console.log(is_rotation(list1, list2));
